97b0c1d78622cf210463ebb9584ae991cc815f14
TAG II <tag_ii@verizon.net>
2011-11-29 02:27:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f400020f584560425233685969ceb0a3b6efc6269bb1fdc5a5a6247d7a882daa0d025fe7bb980856424c006b2c904c3f89657e5af08712fa22c31fd99e5d23f6'
